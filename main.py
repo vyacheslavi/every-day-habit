edc87a8f49179dfce501f10b5fa2b4472996305d
vyacheslavi <vyach.karepov@mail.ru>
2024-05-24 10:03:42 +0700

    add uvicorn

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 from fastapi import FastAPI
+from fastapi.responses import ORJSONResponse
+import uvicorn
 
-app = FastAPI()
+from backend.app.api.api_v1 import router as api_v1_router
+from backend.app.core.config import settings
+
+main_app = FastAPI(
+    responses=ORJSONResponse,
+)
 
 
-@app.get("/")
+@main_app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+main_app.include_router(api_v1_router)
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:main_app",
+        host=settings.run.host,
+        port=settings.run.port,
+        reload=True,
+    )
