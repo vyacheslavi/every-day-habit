8f271668f3c8f241ca7973af3ebe55edd6638064
vyacheslavi <vyach.karepov@mail.ru>
2024-05-31 14:26:24 +0700

    add created ar column to habit table

--- backend/app/database/models/habit.py
+++ backend/app/database/models/habit.py
@@ -1,6 +1,7 @@
+from datetime import datetime
 import sys
 from typing import TYPE_CHECKING
-from sqlalchemy import ForeignKey
+from sqlalchemy import ForeignKey, func
 from sqlalchemy.orm import relationship, Mapped, mapped_column
 
 from .base_model import Base
@@ -16,4 +17,8 @@ class HabitModel(Base):
     name: Mapped[str]
     goal: Mapped[int]
     user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    created_at: Mapped[datetime] = mapped_column(
+        server_default=func.now(),
+        default=func.now(),
+    )
     user: Mapped["UserModel"] = relationship(back_populates="habits")
