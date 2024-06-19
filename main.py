60505d0597132ff68023f29d86ef7749586e05d3
vyacheslavi <vyach.karepov@mail.ru>
2024-06-19 20:50:35 +0700

    refactor main

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+import sys
+from pathlib import Path
+
 from fastapi import FastAPI
+import fastapi
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
@@ -12,35 +16,41 @@ from backend.app.api import api_v1_router
 from backend.app.core.config import settings
 from webapps import router as webapps_router
 
-templates = Jinja2Templates(directory="templates")
 
-main_app = FastAPI(docs_url=None, redoc_url=None)
-main_app.mount("/static", StaticFiles(directory="static"), name="static")
+def initialize_backend_application() -> fastapi.FastAPI:
+
+    templates = Jinja2Templates(directory="templates")
+
+    main_app = FastAPI(docs_url=None, redoc_url=None)
+
+    main_app.mount("/static", StaticFiles(directory="static"), name="static")
+
+    @main_app.get("/docs", include_in_schema=False)
+    async def custom_swagger_ui_html():
+        return get_swagger_ui_html(
+            openapi_url=main_app.openapi_url,
+            title=main_app.title + " - Swagger UI",
+            oauth2_redirect_url=main_app.swagger_ui_oauth2_redirect_url,
+            swagger_js_url="https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js",
+            swagger_css_url="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css",
+        )
+
+    @main_app.get(main_app.swagger_ui_oauth2_redirect_url, include_in_schema=False)
+    async def swagger_ui_redirect():
+        return get_swagger_ui_oauth2_redirect_html()
+
+    main_app.include_router(api_v1_router)
+    main_app.include_router(webapps_router)
+
+    return main_app
 
 
-@main_app.get("/docs", include_in_schema=False)
-async def custom_swagger_ui_html():
-    return get_swagger_ui_html(
-        openapi_url=main_app.openapi_url,
-        title=main_app.title + " - Swagger UI",
-        oauth2_redirect_url=main_app.swagger_ui_oauth2_redirect_url,
-        swagger_js_url="https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js",
-        swagger_css_url="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css",
-    )
-
-
-@main_app.get(main_app.swagger_ui_oauth2_redirect_url, include_in_schema=False)
-async def swagger_ui_redirect():
-    return get_swagger_ui_oauth2_redirect_html()
-
-
-main_app.include_router(api_v1_router)
-main_app.include_router(webapps_router)
+backend_app: fastapi.FastAPI = initialize_backend_application()
 
 
 if __name__ == "__main__":
     uvicorn.run(
-        "main:main_app",
+        "main:backend_app",
         host=settings.run.host,
         port=8000,
         reload=True,
