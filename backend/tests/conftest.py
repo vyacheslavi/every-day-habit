e0c082220e30892af1a9a10644024c36dc25231e
vyacheslavi <vyach.karepov@mail.ru>
2024-08-04 11:49:48 +0700

    add auth client

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,15 +1,16 @@
 import os
-import asyncio
 import fastapi
 import httpx
 import pytest
-from sqlalchemy.ext.asyncio import async_sessionmaker
 
 from backend.app.database.models import Base
 from backend.app.database.db_helper import test_db_helper, db_helper
 
-
+from backend.app.database.models.user import UserModel
 from main import initialize_backend_application
+
+TEST_USER_EMAIL = "test@mail.com"
+TEST_USER_PWD = "12345"
 
 
 @pytest.fixture(name="backend_test_app")
@@ -19,6 +20,20 @@ def backend_test_app() -> fastapi.FastAPI:
     """
 
     return initialize_backend_application()
+
+
+@pytest.fixture(scope="session", autouse=True)
+async def prepare_db():
+
+    async with test_db_helper.engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+
+
+@pytest.fixture
+async def session():
+    async with test_db_helper.session_factory() as session:
+        yield session
 
 
 @pytest.fixture(name="client", scope="function")
@@ -38,23 +53,31 @@ async def client(
         yield client
 
 
-@pytest.fixture(scope="session", autouse=True)
-async def prepare_db():
+@pytest.fixture(name="auth_client", scope="function")
+async def auth_client(
+    client: httpx.AsyncClient,
+):
+    body = {
+        "grant_type": "password",
+        "username": TEST_USER_EMAIL,
+        "password": TEST_USER_PWD,
+        "scope": "",
+        "client_id": "",
+        "client_secret": "",
+    }
+    headers = {
+        "Accept": "application/json",
+        "Content-Type": "application/x-www-form-urlencoded",
+    }
 
-    async with test_db_helper.engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
-        await conn.run_sync(Base.metadata.create_all)
-
-
-@pytest.fixture(scope="function")
-async def session():
-    async with test_db_helper.session_factory() as session:
-        yield session
-
-
-# @pytest.fixture(scope="session")
-# def event_loop(request):
-#     """Create an instance of the default event loop for each test case."""
-#     loop = asyncio.get_event_loop_policy().new_event_loop()
-#     yield loop
-#     loop.close()
+    # When
+    response = await client.post(
+        url="/api/v1/token",
+        data=body,
+        headers=headers,
+    )
+    print(f"{response.text}--------------------------52------------------")
+    access_token = response.cookies.get("access_token")
+    client.cookies.set("access_token", access_token)
+    print(f"{access_token}--------------------------53------------------")
+    yield client
